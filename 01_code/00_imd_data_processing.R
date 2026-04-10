5f8a0129577f94825a87fbe627feabd99578e91a
Rob <morris.robertma@gmail.com>
2026-04-10 17:53:31 +0100

    WTiDS: Electricity Data Clean & Visualisation

--- 01_code/00_imd_data_processing.R
+++ 01_code/00_imd_data_processing.R
@@ -15,6 +15,7 @@ library(data.table)
 # paths
 inp_path_imd <- c("~/06_university/00_university_of_sussex/05_summer_semester/03_wider_topics_in_data_science/00_repo/00_data/00_imd/")
 inp_path_geo <- c("~/06_university/00_university_of_sussex/05_summer_semester/03_wider_topics_in_data_science/00_repo/00_data/xx_geo/")
+out_path <- c("~/06_university/00_university_of_sussex/05_summer_semester/03_wider_topics_in_data_science/00_repo/00_data/xx_clean/")
 
 # imd file names (same as when downloaded from gov.uk)
 file_imd_2025 <- c("File_1_IoD2025_Index_of_Multiple_Deprivation.xlsx")
@@ -76,10 +77,9 @@ dt_07 <- dt_07[order(imd_rank_07)]
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # IMD Data: Clean and Merge
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Drop decile columns
-dt_25 <- dt_25[, imd_decile_25 := NULL]
-dt_19 <- dt_19[, imd_decile_19 := NULL]
-dt_15 <- dt_15[, imd_decile_15 := NULL]
+# Generate decile columns
+dt_07 <- dt_07[, imd_decile_07 := ceiling(.I / (.N / 10))]
+dt_10 <- dt_10[, imd_decile_10 := ceiling(.I / (.N / 10))]
 
 # Drop additional columns from historic data
 dt_10 <- dt_10[,c("la_code", "gor_code", "gor_name", "imd_score_10") := NULL]
@@ -129,22 +129,22 @@ dt_07 <- unique(dt_07, by = c("lsoa_code","imd_rank_07"))
 # Merge Data
 #~~~~~~~~~~~~~~~~~~~~~~~~~
 dt_imd <- merge(dt_25,
-                dt_19[,.(lsoa_code, imd_rank_19)],
+                dt_19[,.(lsoa_code, imd_rank_19, imd_decile_19)],
                 by = 'lsoa_code',
                 all = T)
 
 dt_imd <- merge(dt_imd,
-                dt_15[,.(lsoa_code, imd_rank_15)],
+                dt_15[,.(lsoa_code, imd_rank_15, imd_decile_15)],
                 by = 'lsoa_code',
                 all = T)
 
 dt_imd <- merge(dt_imd,
-                dt_10[,.(lsoa_code, imd_rank_10)],
+                dt_10[,.(lsoa_code, imd_rank_10, imd_decile_10)],
                 by = "lsoa_code",
                 all = T)
 
 dt_imd <- merge(dt_imd,
-                dt_07[,.(lsoa_code, imd_rank_07)],
+                dt_07[,.(lsoa_code, imd_rank_07, imd_decile_07)],
                 by = "lsoa_code",
                 all = T)
 
@@ -176,6 +176,22 @@ dt_imd <- dt_imd[, delta_25_19 := imd_rank_25 - imd_rank_19][
     , delta_15_10 := imd_rank_15 - imd_rank_10][
       , delta_10_07 := imd_rank_10 - imd_rank_07]
 
+# Calculate decile shift relative to 2025 
+dt_imd <- dt_imd[, delta_decile_5y := imd_decile_25 - imd_decile_19][
+  , delta_decile_10y := imd_decile_25 - imd_decile_15][
+    , delta_decile_15y := imd_decile_25 - imd_decile_10][
+      , delta_decile_18y := imd_decile_25 - imd_decile_07]
+
+# Calculate decile shift from previous IMD calc
+dt_imd <- dt_imd[, delta_decile_25_19 := imd_decile_25 - imd_decile_19][
+  , delta_decile_19_15 := imd_decile_19 - imd_decile_15][
+    , delta_decile_15_10 := imd_decile_15 - imd_decile_10][
+      , delta_decile_10_07 := imd_decile_10 - imd_decile_07]
+
+# Migration Flags
+# Delta to Decile - negative values if area became more deprived
+dt_imd <- dt_imd[, dep_flag_5y := ifelse(delta_decile_5y < 0, 1, 0)]
+
 #~~~~~~~~~~~~~~~~~~~~~~~~~
 # IMD Data: Export
 #~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -183,7 +199,7 @@ dt_imd <- dt_imd[, delta_25_19 := imd_rank_25 - imd_rank_19][
 dt_imd <- unique(dt_imd)
 
 # Export
-write.csv(dt_imd, file = paste0(inp_path_imd, "00_imd_data_clean.csv"))
+write.csv(dt_imd, file = paste0(out_path, "00_imd_data_clean.csv"))
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Clean 
--- 01_code/02_energy.R
+++ 01_code/02_energy.R
@@ -1,5 +1,5 @@
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# 01_energy.R
+# 02_energy.R
 # Purpose of Script: Import and process energy data for project
 #                    'Real-Time Monitoring of Local Authority Socioeconomic 
 #                     Position and Deprivation Risk in the UK'
@@ -16,6 +16,7 @@ library(data.table)
 # paths
 inp_path_geo <- c("~/06_university/00_university_of_sussex/05_summer_semester/03_wider_topics_in_data_science/00_repo/00_data/xx_geo/")
 inp_en <- c("~/06_university/00_university_of_sussex/05_summer_semester/03_wider_topics_in_data_science/00_repo/00_data/02_energy/")
+out_path <- c("~/06_university/00_university_of_sussex/05_summer_semester/03_wider_topics_in_data_science/00_repo/00_data/xx_clean/")
 
 # digital file names
 file_energy <- c("LSOA_domestic_elec_2010-2024.xlsx")
@@ -28,9 +29,9 @@ file_geo <- c("geo_map_clean.csv")
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 dt_geo <- as.data.table(read.csv(paste0(inp_path_geo, file_geo)))
 
-# cut down
-dt_geo_cut <- dt_geo[,.(lsoa01, lsoa11, lsoa21)]
-dt_geo_cut <- unique(dt_geo_cut)
+# ensure unique
+dt_geo_cut <- dt_geo[, pcd := NULL]
+dt_geo_cut <- unique(dt_geo)
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Electricity Data: Import
@@ -59,14 +60,16 @@ for (s in sheets){
     setnames(dt_en, gsub("_$", "", names(dt_en)))
 
     # filter
-    dt_en <- dt_en[,.(lsoa_code, number_of_meters,
+    dt_en <- dt_en[,.(local_authority_code, local_authority,
+                      msoa_code, middle_layer_super_output_area,
+                      lsoa_code, number_of_meters,
                       total_consumption_kwh, mean_consumption_kwh_per_meter,
                       median_consumption_kwh_per_meter)]
     
     # organise name
     setnames(dt_en,
-             old = names(dt_en)[-1],
-             new = paste0(names(dt_en)[-1], "_", s_upd))
+             old = names(dt_en)[6:length(names(dt_en))],
+             new = paste0(names(dt_en)[6:length(names(dt_en))], "_", s_upd))
 
     # output
     assign(paste0("dt_en_", s_upd), dt_en)
@@ -76,54 +79,68 @@ for (s in sheets){
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Electricity Data: Merge
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Standardize to lsoa code 2011
+# Standardize to lsoa 21 - currently using 2011 version
 ### 2010 - join via lsoacode01
 dt_en_2010 <- merge(dt_en_2010,
-                    unique(dt_geo_cut[,.(lsoa01, lsoa11)]),
-                    by.x = "lsoa_code",
-                    by.y = "lsoa01",
+                    unique(dt_geo_cut[,.(lsoa11, lsoa21, msoa11, msoa21)]),
+                    by.x = c("lsoa_code","msoa_code"),
+                    by.y = c("lsoa11","msoa11"),
                     all.x = T)
 
-
-
-
-
+dt_en_2010 <- dt_en_2010[,':=' (lsoa_code = ifelse(is.na(lsoa21), lsoa_code,
+                                                     lsoa21),
+                                msoa_code = ifelse(is.na(msoa21), msoa_code,
+                                                     msoa21))][
+                                                       ,':='(lsoa21 = NULL,
+                                                          msoa21 = NULL)]
 
 
 # Merge all data based on lsoa code
 ### 2025 - 2019
-dt <- merge(dt_en_2025,
-            dt_en_2019,
-            by = "lsoa_code",
+dt <- merge(dt_en_2025[,.(lsoa_code, msoa_code, number_of_meters_2025,
+                          total_consumption_kwh_2025, mean_consumption_kwh_per_meter_2025,
+                          median_consumption_kwh_per_meter_2025)],
+            dt_en_2019[,.(lsoa_code, msoa_code, number_of_meters_2019,
+                          total_consumption_kwh_2019, mean_consumption_kwh_per_meter_2019,
+                          median_consumption_kwh_per_meter_2019)],
+            by = c("lsoa_code","msoa_code"),
             all = T)
 
-### 2015
+#### Add 2015
 dt <- merge(dt,
-            dt_en_2015,
-            by = "lsoa_code",
+            dt_en_2015[,.(lsoa_code, msoa_code, number_of_meters_2015,
+                          total_consumption_kwh_2015, mean_consumption_kwh_per_meter_2015,
+                          median_consumption_kwh_per_meter_2015)],
+            by = c("lsoa_code","msoa_code"),
             all = T)
 
-### 2010
-#### join geo data - map msao from 01 to 11
-dt_en_2010 <- merge(dt_en_2010,
-                    dt_geo_cut,
-                    by.x = "lsoa_code",
-                    by.y = "lsoa01",
-                    all.x = T)
-
-#### aggregate data over new msoa code
-dt_en_2010 <- dt_en_2010[, lsoa_code := ifelse(is.na(lsoa11), lsoa_code, lsoa11)]
-dt_en_2010 <- dt_en_2010[, c("lsoa11","lsoa21") := NULL]
-dt_en_2010 <- unique(dt_en_2010)
-
+#### Add 2010 - multiple joins happen
 dt <- merge(dt,
-            dt_en_2010,
-            by = "lsoa_code",
-            all = T)
+            dt_en_2010[,.(lsoa_code, msoa_code, number_of_meters_2010,
+                          total_consumption_kwh_2010, mean_consumption_kwh_per_meter_2010,
+                          median_consumption_kwh_per_meter_2010)],
+            by = c("lsoa_code","msoa_code"),
+            all.x = T)
 
 
+# check missings - less than 1% missing - discard
+### 2015 - 63 discarded
+dt <- dt[!is.na(number_of_meters_2015)] 
 
+### 2019 - 1 discarded
+dt <- dt[!is.na(number_of_meters_2019)]
+
+### 2025 - 2 discarded
+dt <- dt[!is.na(number_of_meters_2025)]
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Electricity Data: Export
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+write.csv(dt, paste0(out_path,"02_energy_clean.csv"))
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Clean
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 dts <- ls(pattern = "^dt_en")
 rm(list = c(dts))
 
